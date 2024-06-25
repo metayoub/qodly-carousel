59e4bb14f20fd70ccee08aa73c228eaebe833a17
Ayoub METWALLI <ayoub.metwalli@4d.com>
2024-06-25 12:07:49 +0100

    fix dots

--- src/components/Carousel/CarouselDots.tsx
+++ src/components/Carousel/CarouselDots.tsx
@@ -8,9 +8,26 @@ interface CarouselDotsProps {
 }
 
 const CarouselDots: FC<CarouselDotsProps> = ({ totalDots, selectedDot, onDotClick }) => {
+  const dotsToShow = () => {
+    if (totalDots <= 5) {
+      return Array.from({ length: totalDots }).map((_, index) => index);
+    }
+
+    const dots = [];
+    if (selectedDot < 2) {
+      dots.push(0, 1, 2, 3, totalDots - 1);
+    } else if (selectedDot > totalDots - 3) {
+      dots.push(0, totalDots - 4, totalDots - 3, totalDots - 2, totalDots - 1);
+    } else {
+      dots.push(0, selectedDot - 1, selectedDot, selectedDot + 1, totalDots - 1);
+    }
+
+    return Array.from(new Set(dots)).sort((a, b) => a - b);
+  };
+
   return (
     <div className="flex justify-center relative bottom-2 hover:bg-black carousel_dots">
-      {Array.from({ length: totalDots }).map((_, index) => (
+      {dotsToShow().map((index) => (
         <div key={index} onClick={() => onDotClick(index)}>
           <div
             className={cn(
